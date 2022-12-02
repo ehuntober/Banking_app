cbcd499f66ba6d79b0f4096a2396538b3909a41e
ehuntober <ehuntercrypto@gmail.com>
2022-12-02 11:12:14 +0000

    updated app

--- app.py
+++ app.py
@@ -67,9 +67,53 @@ def sendmoney():
     if (str(fromAccount["accountNumber"] == str(toAccount["accountNumber"]))):
         flash("You can't send money to yourself!","warning")
         return redirect("http://" + s.getsockname()[0]+ ":3000/")
-    if (float(fromAccount["balance"] ) < float(request.form["amount"])):
-        flash("You don't have enough money to send this amount!", "warning")
-        return redirect("http://" + s.getsockname()[0] + ":3000/")
-    if (float: f)
-        
-            
+    if (float(fromAccount["balance"] ) >= float(request.form["amount"])):
+        file = open("bank_info.txt","r")
+        Lines = file.readlines()
+        fromAccount = {"name": fromAccount["name"],"accountNumber":fromAccount["accountNumber"],"age":fromAccount["age"],"balance":float(fromAccount["balance"]) - float(request.form["amount"])}
+        toAccount = {"name": toAccount["name"],"accountNumber":toAccount["accountNumber"],"age":toAccount["age"],"balance":float(toAccount["balance"]) - float(request.form["amount"])}
+        for line in Lines:
+            if (line.split()[1] == str(fromAccount["accountNumber"])):
+                with open("bank_info.txt", "r+") as f:
+                    d = f.readlines()
+                    f.seek(0)
+                    for i in d:
+                        if i != line:
+                            f.write(i)
+                    f.truncate()
+                line = str(fromAccount["name"]) + " " + str(fromAccount["accountNumber"]) + " " + str(fromAccount['age']) + " " + str(fromAccount["balance"]) + "\n"
+                file = open("bank_info.txt")
+                file.write(line)
+            if(line.split()[1] == str(toAccount["accountNumber"])):
+                with open("bank_info.txt", "r+") as f:
+                    d = f.readlines()
+                    f.seek(0)
+                    for i in d:
+                        if i != line:
+                            f.write(i)
+                line = line = str(toAccount["name"]) + " " + str(toAccount["accountNumber"]) + " " + str(toAccount['age']) + " " + str(toAccount["balance"]) + "\n"
+                file = open("bank_info.txt")
+                file.write(line)
+                
+        print("Money sent from " + fromAccount["name"] + " to " + toAccount["name"])
+        flash("Successfully sent $" + str(float(request.form["amount"])) + " from " + fromAccount["name"] + " to " + toAccount["name"], "success")
+    else:
+        flash("Insufficient funds", "danger")
+        print("Not enough money")
+        return redirect("http://" + s.getsockname()[0] + ':3000/')
+    
+
+
+@app.route("/", methods=["GET","POST"])
+def login_page():
+    if(request.method == "GET"):
+        if (session.get("accountNumber","0")) =="":
+            return render_template("index.html")
+        else:
+            file = open("bank_info.txt")
+            Lines = file.readline()
+            account = None
+            for line in Lines:
+                if(line.split()[1] == str(session.get("accountNumber","0"))):
+                    account = {"name": line.split()[0], "accountNumber": line.split()[1], "age": line.split()[2], "balance": line.split()[3]}
+                    
